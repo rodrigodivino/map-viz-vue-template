8f76fd6f62f581eb4cd7aca3ecce261b0f217585
Rodrigo Divino <rodrigo.sad.lima@gmail.com>
2022-03-11 16:32:33 -0300

    Add prettier

--- src/components/leaflet-map.vue
+++ src/components/leaflet-map.vue
@@ -1,6 +1,6 @@
 <script lang="ts">
 import * as L from "leaflet";
-import {defineComponent} from "vue";
+import { defineComponent } from "vue";
 
 declare interface LeafletMapState {
   map?: L.Map;
@@ -9,38 +9,38 @@ declare interface LeafletMapState {
 export default defineComponent<{}, LeafletMapState>({
   name: "LeafletMap",
 
-  
   data(): LeafletMapState {
     return {
-      map: undefined
+      map: undefined,
     };
   },
   mounted() {
-    this.map = L.map(this.$refs.map as HTMLElement).setView([51.505, -0.09], 13) as L.Map;
+    this.map = L.map(this.$refs.map as HTMLElement).setView(
+      [51.505, -0.09],
+      13
+    ) as L.Map;
 
-    L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}", {
-      attribution: "Map tiles by <a href=\"http://stamen.com\">Stamen Design</a>, <a href=\"http://creativecommons.org/licenses/by/3.0\">CC BY 3.0</a> &mdash; Map data &copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors",
-      subdomains: "abcd",
-      minZoom: 12,
-      maxZoom: 16,
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      ext: "png"
-    }).addTo(this.map);
+    L.tileLayer(
+      "https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}",
+      {
+        attribution:
+          'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        subdomains: "abcd",
+        minZoom: 12,
+        maxZoom: 16,
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        ext: "png",
+      }
+    ).addTo(this.map);
 
     L.control.scale().addTo(this.map);
-  }
+  },
 });
 </script>
 
 <template>
-  <div
-    id="map"
-    ref="map"
-    class="l-fit"
-  />
+  <div id="map" ref="map" class="l-fit" />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
