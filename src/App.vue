7655719484f9c678521b0076ee73640f229818b5
Rodrigo Divino <rodrigo.sad.lima@gmail.com>
2022-03-11 11:10:51 -0300

    Add base css and application.vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,36 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import './assets/base.css';
+import Application from "./components/application.vue";
+
+export default {
+  components: {Application},
+  data() {
+    return {};
+  }
+};
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="app-body">
+    <Application/>
+  </div>
 </template>
 
-<style>
+<style scoped>
+.app-body {
+  width: 100vw;
+  height: 100vh;
+  background-color: #42b983;
+  overflow: hidden;
+}
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
 }
 </style>
---
+++ src/components/application.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  name: "Application"
+}
+</script>
+
+<template>
+  <div class="application">
+    <header class="l-centered header">Header</header>
+    <main class="s-white l-centered main">Main</main>
+    <footer class="l-centered footer">Footer</footer>
+  </div>
+</template>
+
+<style scoped>
+  .l-centered {
+    max-width: 100%;
+    width: 800px;
+    margin: 0 auto;
+  }
+
+  .s-white {
+    background-color: #FFF;
+  }
+
+  .application {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .header {
+    height: 100px;
+    flex-shrink: 0;
+  }
+
+  .main {
+    height: 100%;
+  }
+
+  .footer {
+    height: 20px;
+    flex-shrink: 0;
+  }
+</style>
