4c0600dd3e9ccf7a84f6637a3975bb6f7303038a
Rodrigo Divino <rodrigo.sad.lima@gmail.com>
2022-03-16 09:11:57 -0300

    Wip add teleport to marker pane

--- src/components/leaflet-map.vue
+++ src/components/leaflet-map.vue
@@ -4,6 +4,7 @@ import { defineComponent } from "vue";
 
 declare interface LeafletMapState {
   map?: L.Map;
+  markerPane?: HTMLElement;
 }
 
 export default defineComponent<{}, LeafletMapState>({
@@ -12,6 +13,7 @@ export default defineComponent<{}, LeafletMapState>({
   data(): LeafletMapState {
     return {
       map: undefined,
+      markerPane: undefined,
     };
   },
   mounted() {
@@ -19,6 +21,10 @@ export default defineComponent<{}, LeafletMapState>({
       [51.505, -0.09],
       13
     ) as L.Map;
+
+    this.map.createPane("labelPane").style.zIndex = "850";
+
+    this.markerPane = this.map.getPane("markerPane");
 
     L.tileLayer(
       "https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}",
@@ -34,13 +40,36 @@ export default defineComponent<{}, LeafletMapState>({
       }
     ).addTo(this.map);
 
+    L.tileLayer(
+      "https://stamen-tiles-{s}.a.ssl.fastly.net/toner-labels/{z}/{x}/{y}{r}.{ext}",
+      {
+        attribution:
+          'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        subdomains: "abcd",
+        minZoom: 12,
+        maxZoom: 16,
+        pane: "labelPane",
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        ext: "png",
+      }
+    ).addTo(this.map);
+
     L.control.scale().addTo(this.map);
+
+    console.log(
+      "this.map.getPane('markerPane')",
+      this.map.getPane("markerPane")
+    );
   },
 });
 </script>
 
 <template>
   <div id="map" ref="map" class="l-fit" />
+  <Teleport v-if="markerPane" :to="map.getPane('markerPane')">
+    <span>Hello World</span>
+  </Teleport>
 </template>
 
 <style scoped></style>
